a5b4150d79bb5c3c3465c99d2a315c45dc5569dc
priyankasinghthakuri <priyankasinghthakuri57@gmail.com>
2022-11-15 22:16:15 +0545

    second commit

--- learningexpress.js
+++ learningexpress.js
@@ -1,8 +1,27 @@
 const express = require("express");
-const app = express();
+const path = require("path");
 
-app.get("", (req, res) => {
+const app = express();
+const publicPath = path.join(__dirname, "public");
+
+app.set("view engine", "ejs");
+
+app.get("/home", (req, res) => {
   res.send("Hello, this is Home Page");
+});
+
+app.get("", (_, res) => {
+  const user = {
+    name: "Priyanka Singh Thakuri",
+    email: "priyanka@test123",
+    city: "kathmandu",
+    skills: ["php", "js", "c++"],
+  };
+  res.render("profile", { user });
+});
+
+app.get("/login", (_, res) => {
+  res.render("login");
 });
 
 app.get("/about", (req, res) => {
