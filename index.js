cdfdbf0b6be9464853590e873cbc55ad1d5495a7
priyankasinghthakuri <priyankasinghthakuri57@gmail.com>
2022-11-16 22:56:53 +0545

    first commit

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
-// const http = require("http");
-// const data = require("./data");
+const express = require("express");
+const reqFilter = require("./middleware");
 
-// http
-//   .createServer((req, res) => {
-//     res.writeHead(200, { "Content-Type": "applicationjson" });
-//     res.write(JSON.stringify(data));
-//     res.end();
-//   })
-//   .listen(8000);
+const app = express();
+const route = express.Router();
 
-// const fs = require("fs");
-// const path = require("path");
-// const dirPath = path.join(__dirname, "files");
-// console.log(dirPath);
+// app.use(reqFilter);
 
-// for (i = 0; i < 5; i++) {
-//   fs.writeFileSync(dirPath + "/hello" + i + ".txt", "a text from file");
-// }
+route.use(reqFilter);
 
-// // reading files
-// fs.readdir(dirPath, (err, files) => {
-//   files.forEach((item) => {
-//     console.log("file name is:", item);
-//   });
-// });
-
-const fs = require("fs");
-const path = require("path");
-const dirPath = path.join(__dirname, "crud");
-const filePath = `${dirPath}/data.txt`;
-
-// creating the file
-fs.writeFileSync(filePath, "This is data file");
-
-// reading the file
-fs.readFile(filePath, "utf8", (err, item) => {
-  console.log(item);
+app.get("/", (req, res) => {
+  res.send("Welcome to Home page");
 });
 
-// updating the file with new text
-fs.appendFile(filePath, "This is text file after update", (err) => {
-  if (!err) {
-    console.log("File is updated");
-  }
+route.get("/users", (req, res) => {
+  res.send("Welcome to users page");
+});
+route.get("/contact", (req, res) => {
+  res.send("Welcome to contact page");
 });
 
-// renaming the file
-fs.rename(filePath, `${dirPath}/datas.txt`, (err) => {
-  if (!err) {
-    console.log("File name is updated");
-  }
+app.get("/about", (req, res) => {
+  res.send("Welcome to About page");
 });
 
-// delete the file
-fs.unlinkSync(`${dirPath}/datas.txt`);
+app.use("/", route);
+
+app.listen(5000);
